7b5a82fa42a70367a8bb583018951be559ec7ba9
John Benedict Cayton <caytkid1@gmail.com>
2018-12-03 16:41:17 -0800

    Testinggtg

--- UnitTest/UnitTest_AuthMan.cs
+++ UnitTest/UnitTest_AuthMan.cs
@@ -9,6 +9,9 @@ namespace UnitTest
     [TestClass]
     public class UnitTest_AuthMan
     {
+        /// <summary>
+        /// 
+        /// </summary>
         AuthorizationManager aM = new AuthorizationManager();
         //ClaimStorage cs = new ClaimStorage();
 
