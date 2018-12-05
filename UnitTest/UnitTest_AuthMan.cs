daab3695352e80304b2eb2168da649f4a6375f65
cf2080 <cf2080@gmail.com>
2018-12-05 12:24:52 -0800

    Created testing sample for checking user to user for Authorizations.

--- UnitTest/UnitTest_AuthMan.cs
+++ UnitTest/UnitTest_AuthMan.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using System.Security.Claims;
 using Authorization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelLayer;
 
 namespace UnitTest
 {
     [TestClass]
     public class UnitTest_AuthMan
     {
-        /// <summary>
-        /// blah blah
-        /// </summary>
         AuthorizationManager aM = new AuthorizationManager();
-        //ClaimStorage cs = new ClaimStorage();
+        User u = new User();
 
         [TestMethod]
-        public void TestCheckAccessValid()
+        public void TestCheckAccessClaimInUserValid()
         {
             string claim = "View Documents";
             bool expected = true;
@@ -32,7 +30,7 @@ namespace UnitTest
         }
 
         [TestMethod]
-        public void TestCheckAccessInvalid()
+        public void TestCheckAccessClaimInUserInvalid()
         {
             string claim = "View Documents";
             bool expected = false;
@@ -59,6 +57,80 @@ namespace UnitTest
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod]
+        public void TestCheckUserToUserValid()
+        {
+            // Used as a second user
+            AuthorizationManager aM2 = new AuthorizationManager();
+
+            string claim = "ViewData";
+            bool expected = true;
+            bool actual;
+            string claim2 = "CreateRegUserAccount";
+
+            aM.user.Name = "Luis";
+            aM.user.Role = "System Admin";
+            aM.user.CollectionClaims.Add(claim2);
+
+            aM2.user.Name = "Alexander";
+            aM2.user.Role = "Registered User";
+            aM2.user.CollectionClaims.Add(claim);
+
+            actual = aM.CheckAccess(claim2);
+            Console.WriteLine(actual);
+
+            bool actual2 = true;
+
+            if (actual == true && aM2.user.Role=="Registered User")
+            {
+                actual2 = true;
+            }
+            else
+            {
+                actual2 = false;
+            }
+
+            Console.WriteLine("Actual Value: " + actual2);
+            Assert.AreEqual(expected, actual2);
+
+        }
+
+        [TestMethod]
+        public void TestCheckUserToUserInvalid()
+        {
+            // Used as a second user
+            AuthorizationManager aM2 = new AuthorizationManager();
+
+            string claim = "ViewData";
+            bool expected = false;
+            bool result;
+            string claim2 = "CreateRegUserAccount";
+
+            aM.user.Name = "Luis";
+            aM.user.Role = "Registered User";
+            aM.user.CollectionClaims.Add(claim2);
+
+            aM2.user.Name = "Alexander";
+            aM2.user.Role = "System Admin";
+            aM2.user.CollectionClaims.Add(claim);
+
+            result = aM.CheckAccess(claim2);
+            
+            bool actual = true;
+
+            if (result == true && aM2.user.Role == "Registered User")
+            {
+                actual = true;
+            }
+            else
+            {
+                actual = false;
+            }
+
+            Console.WriteLine("Actual Value: " + actual);
+            Assert.AreEqual(expected, actual);
+
+        }
         /*
         [TestMethod]
         public void TestCheckUsertoUser()
