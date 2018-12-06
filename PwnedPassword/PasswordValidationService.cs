5ea8a55df5dabb504265ccba8a83eff2c21fd761
Luis Meza <Luis.Meza01@student.csulb.edu>
2018-12-05 23:01:46 -0800

    Updated documentation and added PasswordStatus

--- PwnedPassword/PasswordValidationService.cs
+++ PwnedPassword/PasswordValidationService.cs
@@ -10,7 +10,6 @@ namespace PwnedPassword
     public class PasswordValidationService
     {
         private static string url = "https://api.pwnedpasswords.com/range/";
-        //private static string userAgent = "DBate";
         private static readonly HttpClient httpClient = new HttpClient();
 
         public PasswordValidationService()
@@ -18,17 +17,27 @@ namespace PwnedPassword
 
         }
 
-        public async Task<String> CheckPassword(string prefix)
+        /// <summary>
+        /// Will make a request to the pwned passwords api in order to receive passwords  that start with the prefix and their ranges.
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <returns>A Task<String> that contains all hashed passwords that start with the prefix along with their count. </String></returns>
+        public async Task<String> FindPassword(string prefix)
         {
+            //The prefix is added to the end of the url in order to make request to pwned passwords api.
             Uri uri = new Uri(url + prefix);
+
+            //create both HTTP request and response messages
             HttpRequestMessage pwnedRequest = new HttpRequestMessage(HttpMethod.Get, uri);
             HttpResponseMessage pwnedResponse = null;
 
             try
             {
+                //Make request to pwned passwords api.
                 pwnedResponse = await httpClient.SendAsync(pwnedRequest);
                 pwnedResponse.EnsureSuccessStatusCode();
 
+                //Convert response from pwned passwords into a Task<String> and return it.
                 string responseHashList = await pwnedResponse.Content.ReadAsStringAsync();
                 return responseHashList;
 
