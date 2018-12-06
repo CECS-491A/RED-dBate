f8da4c9dded77662b71b63c7f8e8232b70d46159
Deivis Leung <deivis.leung@student.csulb.edu>
2018-12-06 03:11:31 -0800

    Unit testing for creation and deletion

--- UnitTest/UserManagement_CreationTest.cs
+++ UnitTest/UserManagement_CreationTest.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using DataAccessLayer;
 using DataAccessLayer.Mock;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ModelLayer;
 using UserManagement;
+
 
 namespace UnitTest
 {
@@ -14,56 +15,162 @@ namespace UnitTest
 
         IUnitOfWork _uow;
         Creation _creation;
-        //UserManagement_Manager _userManagement;
+        IRepository<User> _User;
 
         [TestInitialize]
         public void SetUp()
         {
             _uow = new MockUnitOfWork<MockDataContext>();
             _creation = new Creation(_uow);
+            _User = _uow.GetRepository<User>();
         }
 
         [TestCleanup]
         public void TearDown()
         {
+
             // Clean resources
         }
 
+        /*
+        /// <summary>
+        /// Test Case for empty creation ATTEMPT
+        /// </summary>
         [TestMethod]
-        public void TestCreateAccountValid()
+        public void Creation_isEmpty()
         {
-
-            bool expected = true;
-            User u = new User();
-            u.ID = 5;
-            u.Name = "Barrack Obama";
-            u.Role = "Registered User";
-            u.Username = "BObama";
-            u.Password = "hope2010";
-            u.DOB = "4/25/1959";
-            u.Location = "Honolulu, Hawaii USA";
-            u.IsAccountActivated = true;
-            u.CollectionClaims = new List<string> { "View" };
-
-            bool actual = _creation.CreateAccount(u);
-            Assert.AreEqual(expected, actual);
-
-
-        }
-
-        [TestMethod]
-        public void TestCreateAccountInvalid()
-        {
+            var uName = " ";
+            var uName2 = " "; 
             bool expected = false;
-            User u = new User();
-            u.ID = 5;
-            u.Name = "Michelle Obama";
-            u.Role = "Registered";
-            u.IsAccountActivated = false;
-            u.CollectionClaims =  new List<string> { "View" };
-            bool actual = _creation.CreateAccount(u);
+
+            User u1 = _User.GetAll().Where(s => s.Username == uName).SingleOrDefault();
+            User u2 = _User.GetAll().Where(s => s.Username == uName2).SingleOrDefault();
+
+            bool actual = _creation.CreateAccount(u1, u2);
             Console.WriteLine(actual);
+
             Assert.AreEqual(expected, actual);
         }
+        */
+
+        /// <summary>
+        /// Test Case duplication
+        /// </summary>
+        [TestMethod]
+        public void Creation_Duplication()
+        {
+            var uName = "Bill2080"; //system admin
+            bool expected = true;
+
+            User u1 = _User.GetAll().Where(s => s.Username == uName).SingleOrDefault();
+
+            bool actual = _creation.Duplication(u1);
+            Console.WriteLine(actual);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        /*
+        /// <summary>
+        /// Test Case duplication  UNABLE TO PASS A NON DUPLICATE WITH A NEW ACC
+        /// </summary>
+        [TestMethod]
+        public void Creation_NotDuplication()
+        {
+            var uName = "Bob"; // user
+            bool expected = false;
+
+            User u1 = _User.GetAll().Where(s => s.Username == uName).SingleOrDefault();
+
+            bool actual = _creation.Duplication(u1);
+            Console.WriteLine(actual);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        */
+
+        /// <summary>
+        /// Test case for creation admin on user
+        /// </summary>
+        [TestMethod]
+        public void Creation_AdminToUser_Valid()
+        {
+            var uName = "Bob2080"; //admin
+            var uName2 = "VicePresident"; //regular user
+            bool expected = true;
+
+            User u1 = _User.GetAll().Where(s => s.Username == uName).SingleOrDefault();
+            User u2 = _User.GetAll().Where(s => s.Username == uName2).SingleOrDefault();
+
+            bool actual = _creation.CreateAccount(u1, u2);
+            Console.WriteLine(actual);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        /// <summary>
+        /// Test case for creation user on admin
+        /// </summary>
+        [TestMethod]
+        public void Creation_UserToAdmin_NotValid()
+        {
+            var uName = "VicePresident"; //regular user
+            var uName2 = "Bob2080"; //admin
+            bool expected = false;
+
+            User u1 = _User.GetAll().Where(s => s.Username == uName).SingleOrDefault();
+            User u2 = _User.GetAll().Where(s => s.Username == uName2).SingleOrDefault();
+
+            bool actual = _creation.CreateAccount(u1, u2);
+            Console.WriteLine(actual);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        /// <summary>
+        /// System admin creating admin
+        /// </summary>
+        [TestMethod]
+        public void Creation_SystemAdminToAdmin_Valid()
+        {
+            var uName = "Bill2080"; //system admin
+            var uName2 = "Bob2080"; //admin
+            bool expected = true;
+
+            User u1 = _User.GetAll().Where(s => s.Username == uName).SingleOrDefault();
+            User u2 = _User.GetAll().Where(s => s.Username == uName2).SingleOrDefault();
+
+            bool actual = _creation.CreateAccount(u1, u2);
+            Console.WriteLine(actual);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        /*
+        /// <summary>
+        /// THIS CODE IS WRONG. ADMIN CREATING SYSTEM ADMIN
+        /// </summary>
+        [TestMethod]
+        public void Creation_AdminToSystemAdmin_Valid()
+        {
+            var uName = "Bob2080"; //admin
+            var uName2 = "Bill2080"; //systemadmin
+            bool expected = false;
+
+            User u1 = _User.GetAll().Where(s => s.Username == uName).SingleOrDefault();
+            User u2 = _User.GetAll().Where(s => s.Username == uName2).SingleOrDefault();
+
+            bool actual = _creation.CreateAccount(u1, u2);
+            Console.WriteLine(actual);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        */
     }
 }
