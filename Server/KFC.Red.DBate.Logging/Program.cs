12f7a426eda2a7e9edd968d393d3d7e637c6745d
Deivis Leung Liang <Deivis.Leung@student.csulb.edu>
2019-03-18 16:07:26 -0700

    Logging implementation

---
+++ Server/KFC.Red.DBate.Logging/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog.Fluent;
+//using DataAccessLayer.NLog.Mongo;
+using NLog;
+//using NLog.Web;
+
+namespace CECS491Logging
+{
+    class Program
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            logger.Error("This is an error message");
+            logger.Trace("This is a telemetry message");
+        }
+    }
+}
