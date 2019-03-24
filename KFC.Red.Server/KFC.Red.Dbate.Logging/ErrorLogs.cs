fe683b84d714db0e139382c88dc18cca2150f729
Deivis Leung <deivis.leung@student.csulb.edu>
2019-03-23 22:18:44 -0700

    Error Logging Update -Added repositories with MongoDB Collections - Update NLog.config targets - Initial query design in Bson format

---
+++ KFC.Red.Server/KFC.Red.Dbate.Logging/ErrorLogs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+/// <summary>
+/// Representing and fomatting the Logs into Bson attributes.
+/// </summary>
+namespace KFC.Red.Dbate.Logging.Repositories
+{
+    public class ErrorLogs
+    {
+        /// <summary>
+        /// Using Bson format to 
+        /// Accessing the id proerty as a string but have the id saveed as an ObjectId in MongoDB.
+        /// </summary>
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ID { get; set; }
+
+        [BsonElement("Date")]
+        public string Date { get; set; }
+
+        [BsonElement("ErrorMessage")]
+        public string ErrorMessage { get; set; }
+
+        [BsonElement("Line of Code")]
+        public string LineOfCode { get; set; }
+
+        [BsonElement("Target Site")]
+        public string TargetSite { get; set; }
+
+        [BsonElement("Logged In User")]
+        public string LoggedInUser { get; set; }
+
+        [BsonElement("ProcessId")]
+        public string ProcessId { get; set; }
+
+        [BsonElement("ProcessName")]
+        public string ProcessName { get; set; }
+
+        [BsonElement("UserRequest")]
+        public string UserRequest { get; set; }
+
+        [BsonElement("UserId")]
+        public string UserId { get; set; }
+
+        [BsonElement("Username")]
+        public string Username { get; set; }
+
+
+    }
+}
---
+++ KFC.Red.Server/KFC.Red.Dbate.Logging/Repositories/MongoDBRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace KFC.Red.Dbate.Logging.Repositories
+{
+    /// <summary>
+    /// File to connect to the Mongo Repository.
+    /// </summary>
+    class MongoDBRepo
+    {
+        //The client that manage the connection
+        public MongoClient Client;
+        //The interface that manage the database
+        public IMongoDatabase Db;
+
+        public MongoDBRepo(string url, string database)
+        {
+            this.Client = new MongoClient(url);
+            // Comment out. (create database if it doenst exist already)
+            this.Db = this.Client.GetDatabase(database);
+        }
+    }
+}
