13354e9d0e934faf34f13d42ecbfd39ed6b90590
cf2080 <cf2080@gmail.com>
2019-03-23 17:00:06 -0700

    Added session and started repository for it

---
+++ KFC.Red.Server/KFC.Red.DataAccessLayer/Models/Session.cs
@@ -0,0 +1,26 @@
+﻿using KFC.Red.DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFC.RED.DataAccessLayer.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+
+        public DateTime CreateTime { get; set; } = DateTime.UtcNow;
+        public DateTime DeleteTime { get; set; } = DateTime.UtcNow;
+        public DateTime UpdateTime { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("UserOfSession")]
+        public int UId { get; set; }
+        public User User { get; set; }
+
+
+    }
+}
