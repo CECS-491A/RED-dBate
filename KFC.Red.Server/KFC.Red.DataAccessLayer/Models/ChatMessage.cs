859ee21e358388d6941b8acb74192799fe746bd3
cf2080 <cf2080@gmail.com>
2019-04-05 15:52:42 -0700

    Started implementation of Hub Service in service layer

---
+++ KFC.Red.Server/KFC.Red.DataAccessLayer/Models/ChatMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFC.RED.DataAccessLayer.Models
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
