22454afe4f025f928eee01c729ff98252f4fcdd3
caytkid1 <32804628+caytkid1@users.noreply.github.com>
2019-04-10 08:35:52 -0700

    Update TelemetryLoggingService.cs

--- KFC.Red.Server/KFC.Red.ServiceLayer/Logging/TelemetryLoggingService.cs
+++ KFC.Red.Server/KFC.Red.ServiceLayer/Logging/TelemetryLoggingService.cs
@@ -8,6 +8,8 @@ using MongoDB.Bson;
 using KFC.RED.DataAccessLayer.Models;
 using KFC.Red.ServiceLayer.Logging.Interfaces;
 using KFC.RED.DataAccessLayer.DTOs;
+using System.Net.Mail;
+using System.Net;
 
 namespace KFC.Red.ServiceLayer.Logging
 {
@@ -16,6 +18,7 @@ namespace KFC.Red.ServiceLayer.Logging
         public MongoClient Client { get; set; }
         public IMongoDatabase documents { get; set; }
         public IMongoCollection<TelemetryLogDTO> _tlogCollection;
+        public int failedLogs;
 
         public TelemetryLoggingService()
         {
@@ -51,7 +54,7 @@ namespace KFC.Red.ServiceLayer.Logging
             myDoc.InsertOne(telemetryLog);
         }
 
-        public void CreateTelemetryLog()
+        public void CreateTelemetryLog(Exception ex)
         {
             TelemetryLogs telemetryLog = new TelemetryLogs();
             BsonDocument log = new BsonDocument();
@@ -59,19 +62,46 @@ namespace KFC.Red.ServiceLayer.Logging
 
             try
             {
-                int zero = 0;
-                int result = 5 / zero;
-            }
-            catch (DivideByZeroException ex)
-            {
                 BsonElement date = new BsonElement("date", telemetryLog.Date);
                 BsonElement error = new BsonElement("error", ex.Message.ToString());
                 BsonElement target = new BsonElement("target", ex.TargetSite.ToString());
                 BsonElement currentLoggedUser = new BsonElement("loggedInUser", "Jane Doe");
                 BsonElement userRequest = new BsonElement("userRequest", "click event");
                 log.Add(date); log.Add(error); log.Add(target); log.Add(currentLoggedUser); log.Add(userRequest);
+
+                myDoc.InsertOne(log);
             }
-            myDoc.InsertOne(log);
+            catch (MongoConnectionException e)
+            {
+                failedLogs++;
+                if(failedLogs <= 100)
+                {
+                    //Email Notification
+                    //https://stackoverflow.com/questions/4677258/send-email-using-system-net-mail-through-gmail/4677382
+                    MailMessage mail = new MailMessage();
+
+                    mail.From = new System.Net.Mail.MailAddress("teamred533@yahoo.com");
+
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential(mail.From.ToString(), "dbate2019!");
+                    smtp.Host = "smtp.mail.yahoo.com";
+
+                    //Replace with admin address
+                    mail.To.Add(new MailAddress("caytkid1@gmail.com"));
+
+                    mail.IsBodyHtml = true;
+                    mail.Subject = "Test Subject";
+                    mail.Body = "Test Message";
+                    smtp.Send(mail);
+
+                    //Reset counter
+                    failedLogs = 0;
+                }
+            }
         }
 
         public void CountTelemetryLog(IMongoCollection<BsonDocument> myDoc, BsonDocument log)
