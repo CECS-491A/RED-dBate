870e0ea4b600e12cfa0a300214e2f7b8bbaaab4c
Deivis Leung <deivis.leung@student.csulb.edu>
2019-04-23 02:01:34 -0700

    archiving setup

---
+++ KFC.Red.Server/KFC.Red.ManagerLayer/Archiving/ArchivingLogManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KFC.Red.ManagerLayer.Archiving
+{
+    public class ArchivingLogManager
+    {
+        //private ArchivingService = new ArchivingService();
+        public ArchivingLogManager()
+        {
+            //ArchiveFolder = new ArchiveFolder();
+        }
+    }
+}
