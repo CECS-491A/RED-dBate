879a211da852c0128344281de00ec9d1d8883118
cf2080 <cf2080@gmail.com>
2019-05-01 01:22:52 -0700

    Backend for Logout sso

---
+++ KFC.Red.Server/KFC.Red.DataAccessLayer/DTOs/LogoutDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFC.RED.DataAccessLayer.DTOs
+{
+    public class LogoutDTO
+    {
+        public string Token { get; set; }
+    }
+}
