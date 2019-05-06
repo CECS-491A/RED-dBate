50cdc421c8c89733914ffbca4d8dd6c2dd6faddc
kmvitug <kvisyummy@yahoo.com>
2019-05-06 16:23:35 -0700

    Implementation of User Profile

---
+++ KFC.Red.Server/KFC.Red.ManagerLayer/UserProfile/UserProfileManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using KFC.Red.DataAccessLayer.Models;
+using KFC.Red.ManagerLayer.UserManagement;
+
+namespace KFC.Red.ManagerLayer.UserProfile
+{
+    public class UserProfileManager
+    {
+        private User user;
+        private UserManager userM;
+
+        public UserProfileManager()
+        {
+            user = new User();
+            userM = new UserManager();
+        }
+
+        public User GetUserInfo(int id)
+        {
+            return userM.GetUser(user.ID);
+        }
+
+        //public User GetUserInfo(string email)
+        //{
+        //    return userM.GetUser(user.Email);
+        //}
+
+        public string DisplayUserEmail(User u)
+        {
+            return u.Email;
+        }
+
+    }
+}
