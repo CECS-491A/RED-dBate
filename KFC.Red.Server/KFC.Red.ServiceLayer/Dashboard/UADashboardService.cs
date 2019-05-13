f8f45c8b8f2e17cda7d0511751cdec4546c56ea0
Deivis Leung <deivis.leung@student.csulb.edu>
2019-05-12 21:23:28 -0700

    uad service

--- KFC.Red.Server/KFC.Red.ServiceLayer/Dashboard/UADashboardService.cs
+++ KFC.Red.Server/KFC.Red.ServiceLayer/Dashboard/UADashboardService.cs
@@ -13,11 +13,30 @@ namespace KFC.Red.ServiceLayer
 {
     public class UADashboardService<T> : IUADashboardService
     {
+        public MongoClient Client { get; set; }
+        /// <summary>
+        /// Built in mongodb method of the mongo database
+        /// </summary>
+        public IMongoDatabase db { get; set; }
+        /// <summary>
+        /// Built in mongodb generic mong
+        /// </summary>
+        public IMongoCollection<T> _logCollection;
+        /// <summary>
+        /// Private collection method to get and set the collection in mongo
+        /// </summary>
+        private string Collection { get; set; }
+
+        /// <summary>
+        /// Logging Service method to set the connection of the mo
+        /// </summary>
+        /// <param name="collectionName"></param>
         public UADashboardService(string collectionName)
         {
-            LoggingService<T> logService = new LoggingService<T>(collectionName);
-
+            Client = new MongoClient("mongodb+srv://RedAdmin:admin123@teamredlogs-r6fsx.azure.mongodb.net/test?retryWrites=true");
+            db = Client.GetDatabase("Logging");
+            _logCollection = db.GetCollection<T>(collectionName);
+            Collection = collectionName;
         }
-
     }
 }
